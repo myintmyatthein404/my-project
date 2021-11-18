2bcaea339599b4381f4cde45217b8be413cabe55
Myint Myat Thein <myintmyatthein404@gmail.com>
2021-11-18 15:54:54 +0630

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f3ca00f17386efc2537022e91382cee2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '337a42e4-4aee-471b-a622-622cbf9466a7'
+};
